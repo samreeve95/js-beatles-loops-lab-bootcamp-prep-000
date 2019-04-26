ed534886332adcd80a518d4b1f3b3ea20642f66d
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 06:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments)
+{
+  
+}
+
+var empty = [];
+
+for (x=0;x<musicians.length;x++)
+{
+  empty.push(`${musicians[x]} plays ${instruments[0]}`);
+}
