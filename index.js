6fdc068462b10fe8edc27fc08925f56b7c087a11
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 06:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,13 @@ function johnLennonFacts(facts)
   var i = 0;
   var clone = facts.slice(0);
   
+  console.log("32");
+  
   while (i < clone.length - 1)
   {
     clone[i] += "!!!";
   }
-  console.log("here");
+  console.log("38");
   return clone;
 }
 
